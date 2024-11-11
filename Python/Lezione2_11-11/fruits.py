70126d5cabfb94f50d5ae80781d33068c44d832f
Raffaele Melluso <raffaele.melluso@edu.unito.it>
2024-11-11 12:53:44 +0100

    esercizio

---
+++ Python/Lezione2_11-11/fruits.py
@@ -0,0 +1,13 @@
+class fruit:
+    def __init__(self, name, color):
+         self.name = name
+         self.color = color
+    def print_info(self):
+         print('This is a', self.color, self.name)
+class tropical_fruit:
+    def __init__(self, name, color, country):
+         super.__init__(name, color)
+         self.country = country
+
+    def print_info(self):
+         print('This is a', self.color, self.name, 'from', self.country)
