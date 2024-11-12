c82f9176aa45cd2d6c6d3ef0b1d3be5dbe2b543c
Raffaele Melluso <raffaele.melluso@edu.unito.it>
2024-11-12 12:15:34 +0100

    python

--- Python/Lezione2_11-11/fruits.py
+++ Python/Lezione2_11-11/fruits.py
@@ -4,10 +4,10 @@ class fruit:
          self.color = color
     def print_info(self):
          print('This is a', self.color, self.name)
-class tropical_fruit:
+class tropical_fruit(fruit):
     def __init__(self, name, color, country):
-         super.__init__(name, color)
-         self.country = country
+        super().__init__(name, color)
+        self.country = country
 
     def print_info(self):
          print('This is a', self.color, self.name, 'from', self.country)
